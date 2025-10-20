5f3132bf20fb60250586ebac1524b6a4d71fc4ae
ebrahkande <ebrahkande@gmail.com>
2025-10-20 15:23:12 -0400

    first commit

---
+++ src/main/java/Room.java
@@ -0,0 +1,47 @@
+public class Room {
+
+    private int numberOfBeds;
+    private double getPrice;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public double getGetPrice() {
+        return getPrice;
+    }
+
+    public void setGetPrice(double getPrice) {
+        this.getPrice = getPrice;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        isDirty = dirty;
+    }
+
+    public boolean isAvailable(){
+        if(!this.isDirty() && !this.isOccupied()){
+            return true;
+        }
+
+        return false;
+    }
+}
